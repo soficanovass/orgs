c14b7d14c090b564f5f0d3497455c0bdfe84dba1
soficanovass <canovasgracindosofia12@gmail.com>
2024-03-21 09:04:05 -0300

    criação da function App

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,17 +1,9 @@
-/**
- * @format
- */
-
-import 'react-native';
 import React from 'react';
-import App from '../App';
-
-// Note: import explicitly to use the types shipped with jest.
-import {it} from '@jest/globals';
-
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-  renderer.create(<App />);
-});
+import {Text} from 'react-native';
+function App(){
+  return(
+    <SafeAreaView></SafeAreaView>
+    <Text> escreve alguma coisa</Text>
+  )
+}
+export default App;
